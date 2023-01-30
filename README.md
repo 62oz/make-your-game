94026957faef723bdf963446744dcd901656c414
62oz <ali.m.ouzir@gmail.com>
2023-01-30 17:24:48 +0200

    wip

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # make-your-game
+
+-> tuto
+https://www.youtube.com/watch?v=3t_SV3WD_98
--- index.js
+++ index.js
@@ -2,6 +2,19 @@
 class GridSystem {
     constructor(matrix) {
         this.matrix = matrix;
+        this.uiContext = this.drawArea(420, 580, "#111")
+        this.outlineContext = this.drawArea(320, 480, "#444")
+        this.topContext = this.drawArea(320, 480, "#111", true)
+        this.cellSize = 30;
+        this.padding = 4;
+    }
+
+
+    getCenter(w, h) {
+        return {
+            x: window.innerWidth / 2 - w / 2 + "px",
+            y: window.innerHeight / 2 - h / 2 + "px"
+        }
     }
 
    /*  What is HTML Canvas?
@@ -15,18 +28,32 @@ Canvas has several methods for drawing paths, boxes, circles, text, and adding i
     //Draw area is used to make canvases that we will have many on top of each other
     // if I understood right
     //Those with = means default value
-    //Flag for transparent or not
-    drawArea(w, h, color = "#111", flag = false) {
+    drawArea(w, h, color = "#111", isTransparent = false) {
         this.canvas = document.createElement("canvas")
+        /* What is context? Context is always the value of the this keyword
+         which is a reference to the object that “owns” the currently
+          executing code or the function where it's looked at. */
         this.context = this.canvas.getContext("2d")
         this.width = this.canvas.width = w;
         this.height = this.canvas.height = h;
-        this.canvas.style.position = "aboslute"
+        this.canvas.style.position = "absolute";
         this.canvas.style.background = color;
+
+        if (isTransparent) {
+            this.canvas.style.backgroundColor = "transparent"; 
+        }
+        //Let's center this:
+        const center = this.getCenter(w, h)
+        this.canvas.style.marginLeft = center.x
+        this.canvas.style.marginTop = center.y
+
+        document.body.appendChild(this.canvas);
+
+        return this.context;
     }
 }
 
-const matrix = [
+const gridMatrix = [
     [1, 1, 1, 1, 1, 1, 1],
     [1, 0, 0, 0, 0, 0, 1],
     [1, 0, 0, 0, 0, 0, 1],
@@ -35,3 +62,5 @@ const matrix = [
     [1, 0, 0, 0, 0, 0, 1],
     [1, 1, 1, 1, 1, 1, 1]
 ];
+
+const gridSystem = new GridSystem(gridMatrix)
