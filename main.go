2144e8055a358f6e60be30a91391602bb4b8ce2e
62oz <ali.m.ouzir@gmail.com>
2023-02-17 02:27:37 +0200

    backup

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"sort"
+)
+
+func main() {
+	// Launch server
+	Server()
+}
+
+func Server() error {
+	// Handle requests
+	http.HandleFunc("/", ScoreHandler)
+	log.Println("Starting server port 8080 (http://localhost:8080/)")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+type ScoreContent struct {
+	// Struct for received scores
+	Name  string `json:"name"`
+	Score int    `json:"score"`
+}
+
+var LeaderBoard []ScoreContent
+
+func ScoreHandler(w http.ResponseWriter, r *http.Request) {
+	// Handle requests
+
+	var NewScore ScoreContent
+
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+
+	if r.Method == "POST" {
+		err := json.NewDecoder(r.Body).Decode(&NewScore)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+	}
+
+	// Add new score to leaderboard
+	LeaderBoard = append(LeaderBoard, NewScore)
+
+	// Sort leaderboard
+	sort.Slice(LeaderBoard, func(i, j int) bool {
+		return LeaderBoard[i].Score > LeaderBoard[j].Score
+	})
+
+	// Only keep top 10 scores
+	if len(LeaderBoard) > 10 {
+		LeaderBoard = LeaderBoard[:10]
+	}
+
+	// Send leaderboard
+	json.NewEncoder(w).Encode(LeaderBoard)
+}
